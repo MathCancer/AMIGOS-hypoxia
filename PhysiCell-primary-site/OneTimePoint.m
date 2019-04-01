bd2d00494b174369bc525f0e76b58315d4b66cde
Furkan Kurtoglu <fkurtog@iu.edu>
2019-04-01 10:14:18 -0400

    Adding Vascularization and update on OneTimePoint.m file

--- PhysiCell-primary-site/OneTimePoint.m
+++ PhysiCell-primary-site/OneTimePoint.m
@@ -4,7 +4,7 @@ clc
 cd C:\Users\Furkan\Documents\GitHub\AMIGOS-hypoxia-Forked-\PhysiCell-primary-site
 cd output\
 
-
+%%
 load('output00000011_microenvironment0.mat');
 XPos = multiscale_microenvironment(1,:);
 YPos = multiscale_microenvironment(2,:);
@@ -14,3 +14,12 @@ plot3(XPos,YPos,Oxygen,'.')
 cd ..
 
 Oxygen(11326)
+%%
+load('output00000000_vasculature.mat');
+XPos = Vascular_Data (1,:);
+YPos = Vascular_Data (2,:);
+FuncVas = Vacular_Data(5,:);
+
+plot3(XPos,YPos,FuncVas,',)
+
+cd ..
